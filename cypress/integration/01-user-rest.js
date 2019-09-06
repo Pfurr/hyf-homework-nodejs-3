ae304d0eeceb8e0e281adaf8a0b5aa0913fbc31d
ideabile <info@ideabile.com>
2019-09-06 21:45:10 +0200

    🧪  Add test for /DELETE user

--- cypress/integration/01-user-rest.js
+++ cypress/integration/01-user-rest.js
@@ -65,4 +65,66 @@ describe('User RESTful', () => {
 
   });
 
+  describe('Delete a User', () => {
+
+    let response;
+    let request;
+    before(async () => {
+
+      request = cy.request({
+        method: 'DELETE',
+        url: '/user/0',
+        followRedirect: false,
+        headers: {
+          'accept': 'application/json'
+        }
+      });
+
+      response = await request;
+
+    });
+
+    it('Should return 202 to the user status', () => {
+
+      expect(response.status).to.eq(202);
+      request.its('body').should('deep.eq', { id: 0 });
+
+    });
+
+    it('The user should be missing from GET /users', () => {
+
+      let getUsers = cy.request({
+        method: 'GET',
+        url: '/users',
+        followRedirect: false,
+        headers: {
+          'accept': 'application/json'
+        }
+      });
+
+      getUsers.then(usersResponse => {
+        expect(usersResponse.status).to.eq(200);
+      });
+
+      getUsers.its('body').should('deep.eq', []);
+
+    });
+
+    it('Should return 204 if user is not found', async () => {
+
+      const missingUser = await cy.request({
+        method: 'DELETE',
+        url: '/user/0',
+        followRedirect: false,
+        headers: {
+          'accept': 'application/json'
+        }
+      });
+
+      expect(missingUser.status).to.eq(204);
+
+    });
+
+  });
+
 });
